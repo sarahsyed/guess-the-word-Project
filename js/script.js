1d9c385f9ec14ae226cf59b7fdefb0abd71d6c33
sarah_syed <sarahsyed2009@gmail.com>
2026-02-01 00:08:40 -0600

    Created a function to show messages according to the player's input

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const textInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuess = document.querySelector(".remaining");
 const span = document.querySelector(".remaining span");
-const message = document.querySelector("message");
+const message = document.querySelector(".message");
 const btnPlayAgain = document.querySelector(".play-again hide");
 
 
@@ -23,8 +23,31 @@ const updateParagraph = function(word){
 updateParagraph("magnolia");
 
 btnGuessIt.addEventListener("click", function(e){
-   e.preventDefault();
-   textValue = textInput.value;
-   textValue = "";
-   
+    e.preventDefault();
+    textValue = textInput.value;
+    playerInput(textInput);
+    //textValue.value = "";
 })
+
+const playerInput = function(textInput){
+    const acceptedLetter = /[a-zA-Z]/;
+    const inputFromPlayer = textInput.value;
+    if (inputFromPlayer === ""){
+        console.log("Input cannot be blank. Please type a letter");
+        message.innerText ="Input cannot be blank. Please type a letter";
+    }
+    else if (inputFromPlayer.length > 1){
+        console.log("You cannot type more than 1 letter");
+        message.innerText = "You cannot type more than 1 letter"
+    }
+    else if (inputFromPlayer.match(acceptedLetter)){
+        message.innerText = `You guessed the letter ${inputFromPlayer}`;
+    }
+    else{
+        console.log("You can only enter a letter");
+         message.innerText = "You can only enter a letter"
+
+    }
+
+}
+
