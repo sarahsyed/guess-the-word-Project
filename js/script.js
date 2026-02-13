38319e9c0e2df1a5db4b148690742080e60253c4
sarah_syed <sarahsyed2009@gmail.com>
2026-02-12 22:31:19 -0600

    Modified getWord function and created a function to fetch a random word. Modified remaining guesses function

--- js/script.js
+++ js/script.js
@@ -6,19 +6,28 @@ const remainingGuess = document.querySelector(".remaining");
 const span = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const btnPlayAgain = document.querySelector(".play-again hide");
-const word = "magnolia"
 const guessedLetters = [];
 var remainingGuesses = 8;
+let word;
+//Picking a random word.
+const selectRandomWord = function(wordArray){
+    return Math.floor(Math.random() * wordArray.length);
+}
+//Fetching words from a file.
 const getWord = async function(){
     const res = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
     const words = await res.text();
     const wordArray = words.split("\n");
-    console.log(wordArray);
-}  
+    let wordIndex = selectRandomWord(wordArray);
+    word = wordArray[wordIndex].trim();
+    updateParagraph(word);
+    console.log(word);
+
+}
 getWord();
 
 
-        const updateParagraph = function(word){
+ const updateParagraph = function(word){
     for (var letter of word){
         var wordArray = [];
         wordArray.push("●");
@@ -27,7 +36,6 @@ getWord();
     }
 }
 
-updateParagraph(word);
 
 //Clicks on Guess it button.
 btnGuessIt.addEventListener("click", function(e){
@@ -57,7 +65,6 @@ const playerInput = function(textValue){
     else{
         // message.innerText = `You guessed the letter ${textValue}`;
          return textValue ;
-
     }
 }
 
@@ -104,26 +111,32 @@ const updateWordInProgress = function(guessedLetters){
         wordInProgress.innerText = updatedWordArray.join("");
         playerResult();
     }
-
 }
 
 const guessRemaining = function(playerInputValue){
         secretWord = word.toUpperCase();
         console.log(secretWord + " " + playerInputValue + " results");
-       if(!secretWord.includes(playerInputValue)){
+    if (!secretWord.includes(playerInputValue)){
         message.innerHTML = "Sorry the word does not contain the letter you typed"
-        remainingGuesses = remainingGuesses - 1;
-        span.innerText = remainingGuesses  + ` guesses`;
-        if (remainingGuesses === 0){
-            message.innerHTML = "Game Over";
-        }
+        remainingGuesses -= 1;
+       // console.log(remainingGuesses);
     }
     else
     {
         message.innerHTML  = "Great! Your guessed letter is in the word";
     }
-    
-}   
+        if (remainingGuesses === 0){
+            message.innerHTML = `Game Over. The word was <span>${word}</span>`;
+        }
+        else if (remainingGuesses === 1){
+           span.innerText = remainingGuesses  + ` guesses`;
+
+        }
+        else {
+           span.innerText = remainingGuesses  + ` guesses`;
+
+        }
+ }   
 
 const playerResult = function(){
     if (wordInProgress.innerText === word.toUpperCase()){
